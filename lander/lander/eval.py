6ef8d3d398a177244b809a807c0887789c76f27e
Cole Wyman <36602061+einwolf@users.noreply.github.com>
2022-09-05 10:48:57 -0400

    Add command line to lander eval

--- lander/lander/eval.py
+++ lander/lander/eval.py
@@ -1,4 +1,6 @@
+import argparse
 import os
+from pathlib import Path
 
 import gym
 from stable_baselines3 import A2C, DQN
@@ -26,14 +28,21 @@ def main():
     """
     make_output_dirs()
 
+    # Parse command line
+    args = parse_cmd_line()
+
+    print(f"{args.iterations=}")
+    print(f"{args.load_model=}")
+
+    # Make environment
     env = gym.make(environment_name)
     env = Monitor(env)
     env = DummyVecEnv([lambda: env])
 
-    print(f"Load best model {dqn_model_path}")
-    model = DQN.load(f"{dqn_model_path}/best_model.zip", env=env)
+    model = DQN.load(args.load_model, env=env)
 
-    mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10, render=True)
+    mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=args.iterations, render=True)
+
     print(f"mean_reward per episode = {mean_reward}")
     print(f"std_reward per episode = {std_reward}")
 
@@ -45,6 +54,18 @@ def main():
     #     env.render()
 
     env.close()
+
+
+def parse_cmd_line():
+    parser = argparse.ArgumentParser(description="Training phase")
+
+    parser.add_argument("--iterations", required=True, type=int,
+                    help="Number of evaluation loops")
+    parser.add_argument("--load_model", required=True, type=Path, default=False,
+                    help="Continue training from this model file")
+
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == "__main__":
